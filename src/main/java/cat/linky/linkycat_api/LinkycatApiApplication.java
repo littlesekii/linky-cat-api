cd01c98472e96a7c010783235616dafd683104f5
littlesekii <contact.sekii@gmail.com>
2025-07-21 21:22:44 -0300

    project initialized

---
+++ src/main/java/cat/linky/linkycat_api/LinkycatApiApplication.java
@@ -0,0 +1,13 @@
+package cat.linky.linkycat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkycatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkycatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/linky/linkycat_api/LinkycatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.linky.linkycat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkycatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
